dc17c42083b1c69ffbd9edcc022082fac099d961
swagat8763 <163417447+swagat8763@users.noreply.github.com>
2024-03-14 18:24:48 +0530

    Final Push

---
+++ FrontEnd/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import AddCategories from "./Components/AddCategories";
+
+function App() {
+  return (
+    <AddCategories/>
+   
+    );
+}
+
+export default App;
